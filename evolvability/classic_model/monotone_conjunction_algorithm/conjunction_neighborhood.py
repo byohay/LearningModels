ea47bf0ee5f6f708e8ddd955261f43881e77c116
Ben Yohay <mooncake91@gmail.com>
2016-01-22 19:53:25 +0200

    removed rep_plus_minus entirely
    it reduces the complexity much.
    The number of generations also get shortend

--- evolvability/classic_model/monotone_conjunction_algorithm/conjunction_neighborhood.py
+++ evolvability/classic_model/monotone_conjunction_algorithm/conjunction_neighborhood.py
@@ -14,31 +14,12 @@ class MonotoneConjunctionNeighborhood(Neighborhood):
         return self.get_rep_plus_and_rep_minus(rep) | self.get_rep_plus_minus(rep)
 
     def get_rep_plus_minus(self, rep):
-        rep_plus_minus = set()
-        rep = list(rep)
-
-        rep_plus_minus.add(tuple(rep))
-
-        index_of_0 = list()
-        index_of_1 = list()
-
-        for i in xrange(len(rep)):
-            if rep[i] == 1:
-                index_of_1.append(i)
-            else:
-                index_of_0.append(i)
-
-        for i in index_of_1:
-            for j in index_of_0:
-                if j <= i:
-                    rep_plus_minus.add(tuple(rep[:j] + [1] + rep[j+1:i] + [0] + rep[i+1:]))
-                else:
-                    rep_plus_minus.add(tuple(rep[:i] + [0] + rep[i+1:j] + [1] + rep[j+1:]))
-
-        return rep_plus_minus
+        return set()
 
     def get_rep_plus_and_rep_minus(self, rep):
         rep_plus_and_rep_minus = set()
+        rep_plus_and_rep_minus.add(tuple(rep))
+
         rep = list(rep)
 
         for i in xrange(len(rep)):
